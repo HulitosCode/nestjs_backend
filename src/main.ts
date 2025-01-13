86fe903eb2300511226deb2aa553382744ba1a58
HulitosCode <heltonmatimbe8@gmail.com>
2025-01-13 22:09:08 +0200

    subindo aplicacao completa

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  //Validar entradas de dados
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  //The ClassSerializerInterceptor uses the class-transformer package to define how to transform objects. Use the @Exclude() decorator to exclude the password field in the UserEntity class:
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+
+    .setTitle('Articles')
+    .setDescription('The Articles API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { Exclude } from 'class-transformer';
+
+export class UserEntity implements User {
+  //The constructor takes an object and uses the Object.assign() method to copy the properties from the partial object to the UserEntity instance. The type of partial is Partial<UserEntity>. This means that the partial object can contain any subset of the properties defined in the UserEntity class.
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  //@ApiProperty decorator will only hide the password property from the Swagger documentation.
+  @Exclude()
+  password: string;
+}
